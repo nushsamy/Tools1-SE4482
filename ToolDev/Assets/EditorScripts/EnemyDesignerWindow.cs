a4512d9813fce1bb0c88eb526142545ab3ed9e6b
Anusha Samarabandu <asamara5@uwo.ca>
2022-09-29 14:47:56 -0400

    finished tutorial

---
+++ ToolDev/Assets/EditorScripts/EnemyDesignerWindow.cs
@@ -0,0 +1,342 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class EnemyDesignerWindow : EditorWindow
+{
+    Texture2D headerSectionTexture;
+    Texture2D mageSectionTexture;
+    Texture2D warriorSectionTexture;
+    Texture2D rogueSectionTexture;
+
+    Color headerSectionColor = new Color(13f / 255f, 32f / 255f, 44f / 255f, 1f);
+
+    Rect headerSection;
+    Rect mageSection;
+    Rect warriorSection;
+    Rect rogueSection;
+
+    static MageData mageData;
+    static WarriorData warriorData;
+    static RogueData rogueData;
+
+    public static MageData MageInfo { get { return mageData; } }
+    public static WarriorData WarriorInfo { get { return warriorData; } }
+    public static RogueData RogueInfo { get { return rogueData; } }
+
+
+    [MenuItem("Window/Enemy Designer")]
+    static void OpenWindow()
+    {
+        EnemyDesignerWindow window = (EnemyDesignerWindow)GetWindow(typeof(EnemyDesignerWindow));
+        window.minSize = new Vector2(600, 300);
+        window.Show();
+    }
+
+    //similar to start or awake
+    private void OnEnable()
+    {
+        InitTextures();
+        InitData();
+    }
+
+    public static void InitData()
+    {
+        mageData = (MageData) ScriptableObject.CreateInstance(typeof(MageData));
+        warriorData = (WarriorData)ScriptableObject.CreateInstance(typeof(WarriorData));
+        rogueData = (RogueData)ScriptableObject.CreateInstance(typeof(RogueData));
+    }
+
+    //initialize texture2D values
+    void InitTextures()
+    {
+        headerSectionTexture = new Texture2D(1, 1);
+        headerSectionTexture.SetPixel(0, 0, headerSectionColor);
+        headerSectionTexture.Apply();
+
+        mageSectionTexture = Resources.Load<Texture2D>("icons/editor_mage_gradient");
+        rogueSectionTexture = Resources.Load<Texture2D>("icons/editor_rogue_gradient");
+        warriorSectionTexture = Resources.Load<Texture2D>("icons/editor_warrior_gradient");
+
+    }
+
+    //similar to any update function. Called 1 or more times per interaction
+    void OnGUI()
+    {
+        DrawLayouts();
+        DrawHeader();
+        DrawMageSettings();
+        DrawWarriorSettings();
+        DrawRogueSettings();
+
+    }
+
+    //Defines Rect values and paints textures based on rects
+    void DrawLayouts()
+    {
+        headerSection.x = 0;
+        headerSection.y = 0;
+        headerSection.width = Screen.width;
+        headerSection.height = 50;
+
+        mageSection.x = 0;
+        mageSection.y = 50;
+        mageSection.width = position.width/3f;
+        mageSection.height = Screen.width - 50;
+
+        warriorSection.x = position.width / 3f;
+        warriorSection.y = 50;
+        warriorSection.width = position.width / 3f;
+        warriorSection.height = Screen.width - 50;
+
+        rogueSection.x = (position.width / 3f) * 2;
+        rogueSection.y = 50;
+        rogueSection.width = position.width / 3f;
+        rogueSection.height = Screen.width - 50;
+
+        GUI.DrawTexture(headerSection, headerSectionTexture);
+        GUI.DrawTexture(mageSection, mageSectionTexture);
+        GUI.DrawTexture(rogueSection, rogueSectionTexture);
+        GUI.DrawTexture(warriorSection, warriorSectionTexture);
+    }
+
+    //draw contents of header
+    void DrawHeader()
+    {
+        GUILayout.BeginArea(headerSection);
+        GUILayout.Label("Enemy Designer");
+
+        GUILayout.EndArea();
+    }
+
+    //draw contents of mage region
+    void DrawMageSettings()
+    {
+        GUILayout.BeginArea(mageSection);
+
+        GUILayout.Label("Mage");
+
+        EditorGUILayout.BeginHorizontal();
+        GUILayout.Label("Damage");
+        mageData.dmgType = (Types.MageDmgType)EditorGUILayout.EnumPopup(mageData.dmgType);
+        EditorGUILayout.EndHorizontal();
+
+        EditorGUILayout.BeginHorizontal();
+        GUILayout.Label("Weapon");
+        mageData.wpnType = (Types.MageWpnType)EditorGUILayout.EnumPopup(mageData.wpnType);
+        EditorGUILayout.EndHorizontal();
+
+        if(GUILayout.Button("Create!", GUILayout.Height(40)))
+        {
+            GeneralSettings.OpenWindow(GeneralSettings.SettingsType.MAGE);
+        }
+
+        GUILayout.EndArea();
+    }
+
+    //draw contents of warrior region
+    void DrawWarriorSettings()
+    {
+        GUILayout.BeginArea(warriorSection);
+
+        GUILayout.Label("Warrior");
+
+        EditorGUILayout.BeginHorizontal();
+        GUILayout.Label("Class");
+        warriorData.classType = (Types.WarriorClassType)EditorGUILayout.EnumPopup(warriorData.classType);
+        EditorGUILayout.EndHorizontal();
+
+
+        EditorGUILayout.BeginHorizontal();
+        GUILayout.Label("Weapon");
+        warriorData.wpnType = (Types.WarriorWpnType)EditorGUILayout.EnumPopup(warriorData.wpnType);
+        EditorGUILayout.EndHorizontal();
+
+        if (GUILayout.Button("Create!", GUILayout.Height(40)))
+        {
+            GeneralSettings.OpenWindow(GeneralSettings.SettingsType.WARRIOR);
+        }
+
+        GUILayout.EndArea();
+    }
+
+    //draw contents of rogue region
+    void DrawRogueSettings()
+    {
+        GUILayout.BeginArea(rogueSection);
+
+        GUILayout.Label("Rogue");
+
+        EditorGUILayout.BeginHorizontal();
+        GUILayout.Label("Weapon");
+        rogueData.wpnType = (Types.RogueWpnType)EditorGUILayout.EnumPopup(rogueData.wpnType);
+        EditorGUILayout.EndHorizontal();
+
+        EditorGUILayout.BeginHorizontal();
+        GUILayout.Label("Strategy");
+        rogueData.strategyType = (Types.RogueStrategyType)EditorGUILayout.EnumPopup(rogueData.strategyType);
+        EditorGUILayout.EndHorizontal();
+
+        if (GUILayout.Button("Create!", GUILayout.Height(40)))
+        {
+            GeneralSettings.OpenWindow(GeneralSettings.SettingsType.ROGUE);
+        }
+
+        GUILayout.EndArea();
+    }
+}
+
+public class GeneralSettings : EditorWindow
+{
+    public enum SettingsType
+    {
+        MAGE,
+        WARRIOR,
+        ROGUE
+    }
+
+    static SettingsType dataSetting;
+    static GeneralSettings window;
+
+    public static void OpenWindow(SettingsType setting)
+    {
+        dataSetting = setting;
+        window = (GeneralSettings)GetWindow(typeof(GeneralSettings));
+        window.minSize = new Vector2(250, 200);
+        window.Show();
+    }
+
+    void OnGUI()
+    {
+        switch (dataSetting)
+        {
+            case SettingsType.MAGE:
+                DrawSettings((CharacterData)EnemyDesignerWindow.MageInfo);
+                break;
+            case SettingsType.WARRIOR:
+                DrawSettings((CharacterData)EnemyDesignerWindow.WarriorInfo);
+                break;
+            case SettingsType.ROGUE:
+                DrawSettings((CharacterData)EnemyDesignerWindow.RogueInfo);
+                break;
+        }
+    }
+
+    void DrawSettings(CharacterData charData)
+    {
+        EditorGUILayout.BeginHorizontal();
+        GUILayout.Label("Prefab");
+        charData.prefab = (GameObject)EditorGUILayout.ObjectField(charData.prefab, typeof(GameObject), false);
+        EditorGUILayout.EndHorizontal();
+
+        EditorGUILayout.BeginHorizontal();
+        GUILayout.Label("Max Health");
+        charData.maxHealth = EditorGUILayout.FloatField(charData.maxHealth);
+        EditorGUILayout.EndHorizontal();
+
+        EditorGUILayout.BeginHorizontal();
+        GUILayout.Label("Max Energy");
+        charData.maxEnergy = EditorGUILayout.FloatField(charData.maxEnergy);
+        EditorGUILayout.EndHorizontal();
+
+        EditorGUILayout.BeginHorizontal();
+        GUILayout.Label("Power");
+        charData.power = EditorGUILayout.Slider(charData.power, 0, 100);
+        EditorGUILayout.EndHorizontal();
+
+        EditorGUILayout.BeginHorizontal();
+        GUILayout.Label("% Crit Chance");
+        charData.critChance = EditorGUILayout.Slider(charData.critChance, 0, charData.power);
+        EditorGUILayout.EndHorizontal();
+
+        EditorGUILayout.BeginHorizontal();
+        GUILayout.Label("Color");
+        charData.colour = EditorGUILayout.ColorField(charData.colour);
+        EditorGUILayout.EndHorizontal();
+
+        EditorGUILayout.BeginHorizontal();
+        GUILayout.Label("Name");
+        charData.name = EditorGUILayout.TextField(charData.name);
+        EditorGUILayout.EndHorizontal();
+
+        if (charData.prefab == null)
+        {
+            EditorGUILayout.HelpBox("This enemy needs a [Prefab] before it can be created.", MessageType.Warning);
+        }
+        else if (charData.name.Length < 1 || charData.name == null)
+        {
+            EditorGUILayout.HelpBox("This enemy needs a [Name] before it can be created.", MessageType.Warning);
+
+        }
+        else if (GUILayout.Button("Finish and Save", GUILayout.Height(30)))
+        {
+            SaveCharacterData();
+        }
+    }
+
+    void SaveCharacterData()
+    {
+        string prefabPath;
+        string newPrefabPath = "Assets/Prefabs/Characters";
+        string dataPath = "Assets/Resources/CharacterData/Data/";
+
+        switch (dataSetting)
+        {
+            case SettingsType.MAGE:
+                dataPath += "Mage/" + EnemyDesignerWindow.MageInfo.name + ".asset";
+                AssetDatabase.CreateAsset(EnemyDesignerWindow.MageInfo, dataPath);
+
+                newPrefabPath += "Mage/" + EnemyDesignerWindow.MageInfo.name + ".prefab";
+
+                prefabPath = AssetDatabase.GetAssetPath(EnemyDesignerWindow.MageInfo.prefab);
+                AssetDatabase.CopyAsset(prefabPath, newPrefabPath);
+                AssetDatabase.SaveAssets();
+                AssetDatabase.Refresh();
+
+                GameObject magePrefab = (GameObject)AssetDatabase.LoadAssetAtPath(newPrefabPath, typeof(GameObject));
+                if (!magePrefab.GetComponent<Mage>())
+                {
+                    magePrefab.AddComponent(typeof(Mage));
+                }
+                magePrefab.GetComponent<Mage>().mageData = EnemyDesignerWindow.MageInfo;
+                break;
+            case SettingsType.WARRIOR:
+                dataPath += "Warrior/" + EnemyDesignerWindow.WarriorInfo.name + ".asset";
+                AssetDatabase.CreateAsset(EnemyDesignerWindow.WarriorInfo, dataPath);
+
+                newPrefabPath += "Warrior/" + EnemyDesignerWindow.WarriorInfo.name + ".prefab";
+
+                prefabPath = AssetDatabase.GetAssetPath(EnemyDesignerWindow.WarriorInfo.prefab);
+                AssetDatabase.CopyAsset(prefabPath, newPrefabPath);
+                AssetDatabase.SaveAssets();
+                AssetDatabase.Refresh();
+
+                GameObject warriorPrefab = (GameObject)AssetDatabase.LoadAssetAtPath(newPrefabPath, typeof(GameObject));
+                if (!warriorPrefab.GetComponent<Warrior>())
+                {
+                    warriorPrefab.AddComponent(typeof(Warrior));
+                }
+                warriorPrefab.GetComponent<Warrior>().warriorData = EnemyDesignerWindow.WarriorInfo;
+                break;
+            case SettingsType.ROGUE:
+                dataPath += "Rogue/" + EnemyDesignerWindow.RogueInfo.name + ".asset";
+                AssetDatabase.CreateAsset(EnemyDesignerWindow.RogueInfo, dataPath);
+
+                newPrefabPath += "Rogue/" + EnemyDesignerWindow.RogueInfo.name + ".prefab";
+
+                prefabPath = AssetDatabase.GetAssetPath(EnemyDesignerWindow.RogueInfo.prefab);
+                AssetDatabase.CopyAsset(prefabPath, newPrefabPath);
+                AssetDatabase.SaveAssets();
+                AssetDatabase.Refresh();
+
+                GameObject roguePrefab = (GameObject)AssetDatabase.LoadAssetAtPath(newPrefabPath, typeof(GameObject));
+                if (!roguePrefab.GetComponent<Rogue>())
+                {
+                    roguePrefab.AddComponent(typeof(Rogue));
+                }
+                roguePrefab.GetComponent<Rogue>().rogueData = EnemyDesignerWindow.RogueInfo;
+                break;
+        }
+    }
+}
---
+++ ToolDev/Assets/Resources/CharacterData/Scripts/CharacterData.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterData : ScriptableObject
+{
+
+    public GameObject prefab;
+    public float maxHealth;
+    public float maxEnergy;
+    public float critChance;
+    public float power;
+    public string name;
+    public Color colour;
+
+}
